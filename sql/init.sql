973de5bb535822b0c0ce69bf663b3eb9aa0b76f0
gangding <gangding@moonton.com>
2025-10-20 11:55:34 +0800

    44445

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,5 @@
 -- 如果存在minigame数据库，则不创建，反之则创建
-CREATE DATABASE IF NOT EXISTS minigame;
+-- CREATE DATABASE IF NOT EXISTS minigame;
 
 -- 创建好友关系的映射表，master_id 和 friend_id
 CREATE TABLE IF NOT EXISTS friend_mapping (

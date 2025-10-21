54d4ee17a2406b2840ef28782a913635eeafed26
gangding <gangding@moonton.com>
2025-10-21 15:57:56 +0800

    44445

--- src/handlers/car.rs
+++ src/handlers/car.rs
@@ -26,7 +26,7 @@ pub async fn change_car(
         }
         None => {
             return (StatusCode::BAD_REQUEST, "房间不存在").into_response();
-        }   
+        }
     }
     match (*state).room_broadcast_couple.get(&request.room_id) {
         Some(couple) => {
@@ -45,7 +45,6 @@ pub async fn change_car(
         }
     }
     (StatusCode::OK, "车辆更换成功").into_response()
-
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -55,32 +54,29 @@ pub struct ChangeCarSkinRequest {
     pub skin_id: i32,
 }
 
-
-
 pub async fn change_car_skin(
     State(state): State<AppState>,
     Json(request): Json<ChangeCarSkinRequest>,
 ) -> impl IntoResponse {
-
     let room_info = (*state).room_info.get_mut(&request.room_id);
     match room_info {
         Some(mut room) => {
             (*room).cars.iter_mut().for_each(|car| {
-                car.skin_id = request.skin_id;
+                if car.car_id == request.car_id {
+                    car.skin_id = request.skin_id;
+                }
             });
             match (*state).room_broadcast_couple.get(&request.room_id) {
-                Some(couple) => {
-                    match couple.0.send(MessageType::Sync(room.clone())) {
-                        Ok(_) => {
-                            debug!("✅ [broadcast_to_ws] 同步消息广播成功");
-                            return (StatusCode::OK, "车辆皮肤更换成功").into_response();
-                        }
-                        Err(e) => {
-                            error!("❌ [broadcast_to_ws] 同步消息广播失败 - 错误: {}", e);
-                            return (StatusCode::BAD_REQUEST, "房间退出失败").into_response();
-                        }
+                Some(couple) => match couple.0.send(MessageType::Sync(room.clone())) {
+                    Ok(_) => {
+                        debug!("✅ [broadcast_to_ws] 同步消息广播成功");
+                        return (StatusCode::OK, "车辆皮肤更换成功").into_response();
                     }
-                }
+                    Err(e) => {
+                        error!("❌ [broadcast_to_ws] 同步消息广播失败 - 错误: {}", e);
+                        return (StatusCode::BAD_REQUEST, "房间退出失败").into_response();
+                    }
+                },
                 None => {
                     return (StatusCode::BAD_REQUEST, "房间不存在").into_response();
                 }
